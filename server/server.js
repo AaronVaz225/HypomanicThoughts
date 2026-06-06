f8585c0a2dc7ebdef6d75a852fd1413c75f5fa85
Aaron Vazquez <aaronrvazquez01@gmail.com>
2026-06-06 11:35:22 -0600

    Added code to connect to MongoDB atlas cluster and created function to start server

--- server/server.js
+++ server/server.js
@@ -1,18 +1,36 @@
 import express from "express";
+import "dotenv/config";
+import mongoose from "mongoose";
+
 
 
 
 const app = express();
-const port = 3000;
 
-app.get("/", (req, res) => {
-    res.send("Hello Worldw!");
-});
 
-app.listen(port, ()=>{
+//Function to connect to Database and Start server
+async function startServer(){
+    try {
+        //Connecting to MongoDB Atlas database cluster
+        await mongoose.connect(process.env.DB_URI);
+        console.log("Database Connected Sucessfully!");
 
-    console.log(`Server running on port ${port}!`);
+        //Starting Server 
+        app.listen(process.env.PORT, ()=>{
+            console.log(`Server running on port ${process.env.PORT}!`);
+        });
+    
 
-});
+    } catch (err) {
+        console.log(`Problem Connecting to Database or Starting server. ${err}`)
+    }
+}
 
-//Test
+startServer();
+
+
+
+
+
+
+
