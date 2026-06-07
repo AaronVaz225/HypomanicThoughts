7ccbeb443f151dc640c35df1c8664ff97ed08cbe
Aaron Vazquez <aaronrvazquez01@gmail.com>
2026-06-07 14:49:42 -0600

    Finished Delete Post api route. Server can now handle creating post, getting all posts, and deleting a post

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -23,3 +23,18 @@ export const getAllPosts = async (req, res) => {
         res.status(500).json({message: "An error occured getting posts."});
     }
 };
+
+//Function to delete post
+export const deletePost = async (req, res) => {
+    try {
+        const deletedPost = await Post.findByIdAndDelete(req.params.id);
+
+        //For some reason, if the user is trying to delete a post that doesn't exist
+        if (!deletedPost) {
+            res.status(404).json({message: "No post with that id found"})
+        }
+        res.status(204).send();
+    } catch (err) {
+        res.status(500).json({message: "An error occured deleting a post"});
+    }
+};
