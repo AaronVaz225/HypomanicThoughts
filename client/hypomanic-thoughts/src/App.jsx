1a770fae42add070224208e130c3fbf38a1f203d
Aaron Vazquez <aaronrvazquez01@gmail.com>
2026-06-08 10:29:10 -0600

    Added basic pages and React Router. No links exist at this point

--- client/hypomanic-thoughts/src/App.jsx
+++ client/hypomanic-thoughts/src/App.jsx
@@ -1,13 +1,20 @@
-
-
-
+import { Routes, Route } from "react-router-dom";
+import Admin from './pages/Admin.jsx';
+import CreatePost from './pages/CreatePost.jsx';
+import Home from './pages/Home.jsx';
+import PostDetails from "./pages/PostDetails.jsx";
 
 
 
 
 const App = () => {
   return (
-    <div>App.jsx</div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/post" element={<PostDetails />} />
+      <Route path="/admin" element={<Admin />} />
+      <Route path="/admin/create-post" element={<CreatePost />} />
+    </Routes>
   )
 }
 
